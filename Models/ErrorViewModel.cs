5b87874f0936162dd5208b94a31022c60f99869d
LEANDRO <crazyg4ng@gmail.com>
2024-11-06 22:04:02 -0300

    Projeto iniciado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VendasWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
